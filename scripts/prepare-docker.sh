b1ae8de2c01f4e622f325f2f3b0b1d26b655db53
xoviat <xoviat@users.noreply.github.com>
2017-09-22 11:46:37 -0500

    scripts: prepare-docker: add symbolic link

--- scripts/prepare-docker.sh
+++ scripts/prepare-docker.sh
@@ -1,3 +1,4 @@
 DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
 docker pull $DOCKER_IMAGE
+ln -s $PWD/.ccache $HOME/.ccache
 docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/scripts/prepare-linux.sh
