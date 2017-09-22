d511060c8d6ea7a26816ac75b04ad61119b07196
xoviat <xoviat@users.noreply.github.com>
2017-09-22 12:16:52 -0500

    scripts: prepare-docker: create ccache directory

--- scripts/prepare-docker.sh
+++ scripts/prepare-docker.sh
@@ -1,4 +1,6 @@
+mkdir -p $HOME/.ccache
+
 DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
 docker pull $DOCKER_IMAGE
-ln -s $PWD/.ccache $HOME/.ccache
-docker run --rm -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/scripts/prepare-linux.sh
+
+docker run --rm -v ${HOME}/.ccache:/ccache -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/scripts/prepare-linux.sh
--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -3,8 +3,8 @@ set -e -x
 
 PYBIN=cp36-cp36m
 
-# Copy the ccache files
-cp -r /io/.ccache $HOME/.ccache
+# Link ccache
+ln -s /ccache $HOME/.ccache
 
 # Activate the virtualenv
 /opt/python/cp36-cp36m/bin/python -m venv venv
@@ -18,10 +18,10 @@ DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
 CMAKE_DIR="${DEPS_DIR}/cmake"
 mkdir -p ${DEPS_DIR}
 CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.2-Linux-x86_64.tar.gz"
-mkdir $CMAKE_DIR && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C $CMAKE_DIR
+mkdir $CMAKE_DIR && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
 
-# Copy the requirements
-cp /io/requirements-dev.txt .
+# Change directories
+cd /io/
 
 source scripts/build.sh
