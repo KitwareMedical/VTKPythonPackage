7fe0edba580d8234bfea98deee57db0ebe56c956
xoviat <xoviat@users.noreply.github.com>
2017-09-22 13:25:32 -0500

    scripts: prepare-docker: know platform

--- scripts/prepare-docker.sh
+++ scripts/prepare-docker.sh
@@ -1,6 +1,10 @@
 mkdir -p $HOME/.ccache
 
-DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
-docker pull $DOCKER_IMAGE
+if [ $TARGET_ARCH = "x86" ]; then
+   DOCKER_IMAGE=quay.io/pypa/manylinux1_i686
+else
+   DOCKER_IMAGE=quay.io/pypa/manylinux1_x86_64
+fi
 
-docker run --rm -v ${HOME}/.ccache:/ccache -v `pwd`:/io $DOCKER_IMAGE $PRE_CMD /io/scripts/prepare-linux.sh
+docker pull $DOCKER_IMAGE
+docker run --rm -v ${HOME}/.ccache:/ccache -v `pwd`:/io $DOCKER_IMAGE -e TARGET_ARCH=$TARGET_ARCH /io/scripts/prepare-linux.sh
