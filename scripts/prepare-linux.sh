3c3af2f7ac1c296ebf8cdf6ba64fd15aae2b66dc
xoviat <xoviat@users.noreply.github.com>
2017-09-22 11:55:07 -0500

    scripts: prepare-linux: call build

--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -21,7 +21,4 @@ CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.2-Linux-x86_64.tar.gz"
 mkdir $CMAKE_DIR && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C $CMAKE_DIR
 export PATH=$CMAKE_DIR/bin:$PATH
 
-# Prove that the environment is ready to go
-cmake --version
-python --version
-pip --version
+source scripts/build.sh
