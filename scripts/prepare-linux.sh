4f64ec4e470215b18e75d92cac007eb1c10fc5e2
xoviat <xoviat@users.noreply.github.com>
2017-09-22 11:38:59 -0500

    scripts: add prepare-linux

---
+++ scripts/prepare-linux.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -e -x
+
+PYBIN=cp36-cp36m
+
+# Copy the ccache files
+cp -r /io/.ccache $HOME/.ccache
+
+# Activate the virtualenv
+/opt/python/cp36-cp36m/bin/python -m venv venv
+source venv/bin/activate
+
+# Install ccache
+yum install -y ccache
+
+# Install a newer CMake
+DEPS_DIR="${TRAVIS_BUILD_DIR}/deps"
+CMAKE_DIR="${DEPS_DIR}/cmake"
+mkdir -p ${DEPS_DIR}
+CMAKE_URL="https://cmake.org/files/v3.9/cmake-3.9.2-Linux-x86_64.tar.gz"
+mkdir $CMAKE_DIR && travis_retry wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C $CMAKE_DIR
+export PATH=$CMAKE_DIR/bin:$PATH
+
+# Prove that the environment is ready to go
+cmake --version
+python --version
+pip --version
