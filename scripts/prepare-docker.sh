0001a18974b26b2d2bee6e9e71b5f01ae7ee8292
xoviat <xoviat@users.noreply.github.com>
2017-09-24 16:07:19 -0500

    scripts: prepare-docker: fix

--- scripts/prepare-docker.sh
+++ scripts/prepare-docker.sh
@@ -10,5 +10,5 @@ fi
 docker pull $DOCKER_IMAGE
 docker run --rm -v ${HOME}/.ccache:/ccache -v ${HOME}/.cache:/cache -v `pwd`:/io \
    -e TRAVIS_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION \
-   -e PYTHON_TAG=$PYTHON_TAG
+   -e PYTHON_TAG=$PYTHON_TAG \
    -e TARGET_ARCH=$TARGET_ARCH $DOCKER_IMAGE /io/scripts/prepare-linux.sh
