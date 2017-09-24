0795afa3beb7f5f0f81dee9fa389b0cc0fecda6e
xoviat <xoviat@users.noreply.github.com>
2017-09-23 21:08:48 -0500

    scripts: prepare-linux: install cmake from pip

--- scripts/prepare-linux.sh
+++ scripts/prepare-linux.sh
@@ -34,16 +34,7 @@ export PATH=$CCACHE_DIR:$PATH
 which gcc
 
 # Install a newer CMake
-if [ $TARGET_ARCH == "x64" ]; then
-  CMAKE_URL="https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz"  
-else
-  CMAKE_URL="https://cmake.org/files/v3.5/cmake-3.5.2-Linux-i386.tar.gz"  
-fi
-
-CMAKE_DIR="${DEPS_DIR}/cmake"
-mkdir -p ${DEPS_DIR}
-mkdir $CMAKE_DIR && wget --no-check-certificate --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C $CMAKE_DIR
-export PATH=$CMAKE_DIR/bin:$PATH
+pip install cmake
 
 # Change directories
 cd /io/
