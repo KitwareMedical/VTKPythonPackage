b5e04ffb1356b66eb0b1a57ca255eb32c5430754
xoviat <xoviat@users.noreply.github.com>
2017-09-24 15:40:05 -0500

    scripts: prepare-docker: pass through entire cache directory

--- scripts/prepare-docker.sh
+++ scripts/prepare-docker.sh
@@ -1,4 +1,5 @@
 mkdir -p $HOME/.ccache
+mkdir -p $HOME/.cache
 
 if [ $TARGET_ARCH = "x86" ]; then
    DOCKER_IMAGE=quay.io/pypa/manylinux1_i686
@@ -7,6 +8,7 @@ else
 fi
 
 docker pull $DOCKER_IMAGE
-docker run --rm -v ${HOME}/.ccache:/ccache -v ${HOME}/.cache/pip:/pip-cache -v `pwd`:/io \
+docker run --rm -v ${HOME}/.ccache:/ccache -v ${HOME}/.cache:/cache -v `pwd`:/io \
    -e TRAVIS_PYTHON_VERSION=$TRAVIS_PYTHON_VERSION \
+   -e PYTHON_TAG=$PYTHON_TAG
    -e TARGET_ARCH=$TARGET_ARCH $DOCKER_IMAGE /io/scripts/prepare-linux.sh
