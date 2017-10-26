35f61ec1a42467e776def71e137994ad458e94d2
xoviat <xoviat@users.noreply.github.com>
2017-10-26 17:27:40 -0500

    [config] build in root of project

--- config.sh
+++ config.sh
@@ -1,5 +1,6 @@
-
+function timeout() { 
+  perl -e 'alarm shift; exec @ARGV' "$@"; 
+}
 function pre_build {
-  export PYTHON_LIBRARY=$(cd $(dirname $0); pwd)/libpython-not-needed-symbols-exported-by-interpreter
-  touch ${PYTHON_LIBRARY}
+  timeout 2400 python setup.py build  
 }
